ab8586956bb118a0d5a651cb4c42c41f04c0e9c9
Sylvestre <sylvestre.gug@gmail.com>
2025-01-16 18:24:03 -0500

    Add search endpoints for Google and DuckDuckGo
    Add endpoints for Google and DuckDuckGo search functionality and set up Swagger documentation.
    
    * Add `@nestjs/swagger` and `swagger-ui-express` dependencies in `package.json`.
    * Create `src/swagger.ts` to configure Swagger documentation.
    * Modify `src/main.ts` to set up Swagger in the `bootstrap` function.
    * Add `SerpService` to `AppController` in `src/app.controller.ts` and create endpoints for Google and DuckDuckGo search.
    * Add tests for the new search endpoints in `src/app.controller.spec.ts`.
    * Add `SerpService` to the `providers` array in `src/app.module.ts`.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/Sylvestre67/gh-workspace?shareId=XXXX-XXXX-XXXX-XXXX).

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SerpModule } from './serp/serp.module';
 import { ConfigModule } from './config/config.module';
+import { SerpService } from './serp/serp.service';
 
 @Module({
   imports: [SerpModule, ConfigModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, SerpService],
 })
 export class AppModule {}
