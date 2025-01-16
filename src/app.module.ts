4cc79cd1ef42af85f0b52c96fab3a50bae08eb92
Sylvestre Gug <sylvestre.gug@gmail.com>
2025-01-16 17:27:18 -0500

    serp module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SerpModule } from './serp/serp.module';
 
 @Module({
-  imports: [],
+  imports: [SerpModule],
   controllers: [AppController],
   providers: [AppService],
 })
